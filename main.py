ecdc8d1adaf20103101a3e67f73bd6addf2fd7d8
Mouheb Kraiem <mouheb.pro.work@gmail.com>
2024-08-29 20:09:51 +0200

    Port 80

--- main.py
+++ main.py
@@ -119,5 +119,5 @@ if __name__ == "__main__":
     import uvicorn
     import os
     
-    port = int(os.environ.get("PORT", 8000))
+    port = int(os.environ.get("PORT", 80))
     uvicorn.run("main:app", host="0.0.0.0", port=port, reload=True)
