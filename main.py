90664762cf0c269bf3d995041d287e18c82aad9b
Mouheb Kraiem <mouheb.pro.work@gmail.com>
2024-08-29 20:02:35 +0200

    Port add

--- main.py
+++ main.py
@@ -117,4 +117,7 @@ def get_country(ip_address: str) -> str:
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    import os
+    
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
